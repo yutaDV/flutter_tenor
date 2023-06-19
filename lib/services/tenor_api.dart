071df4d9abaaca3d75aa26100d07a547854798ee
Yuliia <yutadv@gmail.com>
2023-06-19 23:44:48 +0300

    dio

--- lib/services/tenor_api.dart
+++ lib/services/tenor_api.dart
@@ -1,55 +1,63 @@
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'dart:convert';
 
 class TenorAPI {
+  static Dio _dio = Dio();
+
   static Future<List<String>> searchImages(String searchText) async {
     String apiKey = 'LIVDSRZULELA';
-    String locale = 'en_US'; // Вказати потрібну локаль
+    String locale = 'en_US';
     int limit = 20; // Вказати потрібну кількість результатів
 
     String url = 'https://g.tenor.com/v1/search?key=$apiKey&q=$searchText&locale=$locale&limit=$limit';
 
-    var response = await http.get(Uri.parse(url));
-    var responseBody = json.decode(response.body) as Map<String, dynamic>;
+    try {
+      Response response = await _dio.get(url);
+      if (response.statusCode == 200) {
+        List<dynamic> results = response.data['results'];
 
-    if (response.statusCode == 200) {
-      List<dynamic> results = responseBody['results'];
+        List<String> imageUrls = [];
+        for (var result in results) {
+          String imageUrl = result['media'][0]['gif']['url'];
+          imageUrls.add(imageUrl);
+        }
 
-      List<String> imageUrls = [];
-      for (var result in results) {
-        String imageUrl = result['media'][0]['gif']['url'];
-        imageUrls.add(imageUrl);
+        return imageUrls;
+      } else {
+        print('Error: ${response.statusCode}');
+        throw Exception('Failed to fetch image URLs');
       }
-
-      return imageUrls;
-    } else {
-      print('Error: ${response.statusCode}');
+    } catch (e) {
+      print('Error: $e');
       throw Exception('Failed to fetch image URLs');
     }
   }
 
   static Future<List<String>> searchSuggestions(String searchText) async {
     String apiKey = 'LIVDSRZULELA';
-    String locale = 'en_US'; // Вказати потрібну локаль
+    String locale = 'en_US';
     int limit = 20; // Вказати потрібну кількість результатів
 
     String url = 'https://g.tenor.com/v1/autocomplete?q=$searchText&key=$apiKey&locale=$locale&limit=$limit';
 
-    var response = await http.get(Uri.parse(url));
-    var responseBody = json.decode(response.body) as Map<String, dynamic>;
+    try {
+      Response response = await _dio.get(url);
+      if (response.statusCode == 200) {
+        List<dynamic> suggestions = response.data['results'];
 
-    if (response.statusCode == 200) {
-      List<dynamic> suggestions = responseBody['results'];
+        List<String> suggestionList = [];
+        for (var suggestion in suggestions) {
+          String suggestionText = suggestion['term'];
+          suggestionList.add(suggestionText);
+        }
 
-      List<String> suggestionList = [];
-      for (var suggestion in suggestions) {
-        String suggestionText = suggestion['term'];
-        suggestionList.add(suggestionText);
+        return suggestionList;
+      } else {
+        print('Error: ${response.statusCode}');
+        throw Exception('Failed to fetch search suggestions');
       }
-
-      return suggestionList;
-    } else {
-      print('Error: ${response.statusCode}');
+    } catch (e) {
+      print('Error: $e');
       throw Exception('Failed to fetch search suggestions');
     }
   }
